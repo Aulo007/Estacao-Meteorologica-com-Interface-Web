e1117d07623f20d1f7e1d47df9a258d5d10c18e2
Aulo Cezar <Aulofferreira@gmail.com>
2025-07-21 17:16:12 -0300

    Otimizar o armazenamento HTML e adicionar suporte a bitmap SSD1306
    Move a string da página HTML para a memória flash usando __in_flash para otimização da RAM. Adiciona ssd1306_draw_bitmap ao driver SSD1306 para renderização de bitmap. Atualiza o main.c para configurar e limpar a tela na inicialização. Reduz o tamanho do pool de memória lwIP para gerenciamento de recursos.

--- main.c
+++ main.c
@@ -237,6 +237,12 @@ int main()
     gpio_set_function(I2C_SCL_DISP, GPIO_FUNC_I2C);
     ssd1306_t ssd;
     ssd1306_init(&ssd, 128, 64, false, endereco, I2C_PORT_DISP);
+    ssd1306_config(&ssd);    // Configura o display
+    ssd1306_send_data(&ssd); // Envia os dados para o display
+
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
 
     i2c_init(I2C_PORT, 400 * 1000);
     gpio_set_function(I2C_SDA, GPIO_FUNC_I2C);
